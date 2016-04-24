d5403db8673209dbae57e30460ac3b416267859b
PatrickFant <pbfant@bu.edu>
2016-04-24 01:00:41 -0400

    moved crossRatio function to its own module

--- transformation.py
+++ transformation.py
@@ -27,20 +27,6 @@ def findTf(q1, q2=None):
         return np.multiply(q1[:-1], x).T
 
 
-def crossRatio(p1, p2, p3, p4):
-    """Finds the cross ratio of four points.
-    
-    
-    Keyword arguments:
-    p1 -- the first point
-    p2 -- the second point
-    p3 -- the third point
-    p4 -- the fourth point
-    """
-    a, b = np.linalg.lstsq(np.matrix([p1, p2]).T, np.matrix(p3).T)[0]
-    c, d = np.linalg.lstsq(np.matrix([p1, p2]).T, np.matrix(p4).T)[0]
-    return ((b / a) / (d / c))[(0, 0)] # Unpack result from numpy matrix.
-
 def inv(A):
     """Returns a numpy matrix representing the inverse of A.
     
